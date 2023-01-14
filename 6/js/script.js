fdbc400a5504c75e8ede7a295b979923f6223b57
Keks <keks@htmlacademy.ru>
2023-01-14 13:08:37 +0300

    :heavy_check_mark: Сборка #6

--- 6/js/script.js
+++ 6/js/script.js
@@ -1,31 +1,15 @@
-// let showMenu = document.querySelector(".header__nav-toogle");
-// let mobMenu = document.querySelector(".main-nav");
-// showMenu.addEventListener('click', function () {
-//   if (mobMenu.style.display = 'none') {
-//     mobMenu.style.display = 'block';
-//   } else {
-//     mobMenu.style.display = 'none';
-//   }
-// });
-
-
-
-
-
-let navMain = document.querySelector('.main-nav');
-let navToggle = document.querySelector('.header__nav-toogle');
-
-navMain.classList.remove('main-nav--nojs');
-
-navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-    navToggle.style.backgroundImage = "url('../img/close-menu--mob.svg')";
-
-  } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-    navToggle.style.backgroundImage = "url('../img/burger-menu-mob.svg')";
-  }
-});
+(function () {
+  let navToogle = document.querySelector('.header__nav-toogle');
+  let navMain = document.querySelector('.main-nav');
+  navToogle.addEventListener('click', () => {
+    if (navMain.classList.contains('main-nav--closed')) {
+      navToogle.classList.toggle('header__nav-toogle--active');
+      navMain.classList.remove('main-nav--closed');
+      navMain.classList.add('main-nav--opened');
+    } else {
+      navMain.classList.add('main-nav--closed');
+      navMain.classList.remove('main-nav--opened');
+      navToogle.classList.remove('header__nav-toogle--active');
+    }
+  })
+}());
